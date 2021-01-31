975d657a4bc260ab9c3d38c4190780b5d6d247df
Jelle <jellemaarten02@gmail.com>
2021-01-31 17:17:07 +0100

    update

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,17 @@
+<nav class="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <h4><a href="/">NWAVE</a></h4>
+            </div>
+            <div class="col-9">
+                <ul>
+                    <li><a href="">Over ons</a></li>
+                    <li><a href="">Projecten</a></li>
+                    <li><a href="">Offerte</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
