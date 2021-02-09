109bcb2614ffb184fcac57a32c3254129b78a9fb
Jelle <jellemaarten02@gmail.com>
2021-02-09 18:05:28 +0100

    update

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -45,7 +45,7 @@
                             </div>
                             <div class="col-xl-8 col-12">
                                 <div class="form-parent">
-                                    <form>
+                                    <form method="post" action="{{}}">
                                         <div class="row">
                                             <div class="col-md-6 col-12 form-item">
                                                 <label for="voornaam">Voornaam*</label>
