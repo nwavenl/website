147eaebcce848c5a621e859618eac131afe045ad
Jelle <jellemaarten02@gmail.com>
2021-02-10 19:26:18 +0100

    update contact form

--- resources/views/mail/contact.blade.php
+++ resources/views/mail/contact.blade.php
@@ -1,2 +1,7 @@
-<h1>Dit is de contact email</h1>
-{{ $data['test'] }}
+<div>
+    <h3>Mail van {{ $data['fname'] }}  {{ $data['lname'] }}</h3>
+    <h4>Email: {{ $data['email'] }}</h4>
+    <h4>Nummer: {{ $data['phone'] }}</h4>
+    <br>
+    <p>{{ $data['message'] }}</p>
+</div>
