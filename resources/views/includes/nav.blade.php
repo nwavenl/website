7a62b3a1c24271efcb4feaa2edf33200fc585682
Jelle <jellemaarten02@gmail.com>
2021-02-16 13:45:38 +0100

    update

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-3 col-9 branding">
-                <h4><img src="{{asset('/images/global/logo.png')}}" alt=""><a href="/">NWAVE</a></h4>
+                <a href="/"><img src="{{asset('/images/global/logo.png')}}"><h4>NWAVE</h4></a>
             </div>
             <div class="col-lg-9 col-3 navigation">
                 <div class="desktop">
