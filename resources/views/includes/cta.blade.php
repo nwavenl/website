9c9e0957ef571d30e781022c90f0523b85aeaaf0
Jelle Stekelenburg <jellemaarten02@gmail.com>
2021-05-31 10:11:00 +0200

    Update cta.blade.php

--- resources/views/includes/cta.blade.php
+++ resources/views/includes/cta.blade.php
@@ -6,7 +6,7 @@
                 <div class="cta-content">
                     <h2 data-aos="fade-up" class="main">Geïnteresseerd?</h2>
                     <h2 data-aos="fade-up">Bekijk ons werk.</h2>
-                    <p data-aos="fade-up">Bekijk ons werk en krijg een idruk op ons maatwerk.</p>
+                    <p data-aos="fade-up">Bekijk ons werk en krijg een indruk op ons maatwerk.</p>
                     <a data-aos="fade-up" class="btn hollow" href="/portfolio">Portfolio</a>
                 </div>
             </div>
